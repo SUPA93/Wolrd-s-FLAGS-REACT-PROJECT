88a123889d404b25236b485bae14449ab1910c61
SUPA93 <supaflavaz93@hotmail.com>
2024-02-01 10:17:22 +0100

    New Project

---
+++ src/components/Countries.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import Card from './Card';
+import { Link } from 'react-router-dom';
+
+const Countries = () => {
+    const [data, setData] = useState([])
+    const [rangeValue, setRangeValue] = useState(36);
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all").then((res) => setData(res.data));
+    }, [])
+
+    return (
+        <div className='countries'>
+            <ul className="radio-container">
+                <input
+                    type="range"
+                    min="1" max="250"
+                    defaultValue={rangeValue}
+                    onChange={(e) => setRangeValue(e.target.value)}
+                />
+                {radios.map((continent) =>
+                <li>
+                    <input type="radio" id={continent} name="continentRadio" />
+                <label htmlFor={continent}>{continent}</label>
+                </li>
+                )}
+                
+            </ul>
+            <ul>
+                {data.slice(0, rangeValue).map((country, index) => (
+                    <Card key={index} country={country} />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Countries;
