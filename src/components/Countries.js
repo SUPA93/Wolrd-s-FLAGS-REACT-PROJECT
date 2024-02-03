af42610de2d75b669cafd4154cbdba20b3b040d6
SUPA93 <supaflavaz93@hotmail.com>
2024-02-03 19:57:12 +0100

    Nothing much

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const Countries = () => {
     const [data, setData] = useState([])
     const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
     const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all").then((res) => setData(res.data));
@@ -20,16 +21,27 @@ const Countries = () => {
                     defaultValue={rangeValue}
                     onChange={(e) => setRangeValue(e.target.value)}
                 />
-                {radios.map((continent) =>
-                <li>
-                    <input type="radio" id={continent} name="continentRadio" />
-                <label htmlFor={continent}>{continent}</label>
-                </li>
-                )}
-                
+                {radios.map((continent) => (
+                    <li>
+                        <input type="radio"
+                        id={continent}
+                        name="continentRadio"
+                        checked={continent=== selectedRadio}
+                        onChange={(e) => setSelectedRadio(e.target.id)} 
+                        />
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
             </ul>
+            {selectedRadio && (
+                <button onClick={()=> setSelectedRadio("")}>Annuler la recherche</button>
+            )}
             <ul>
-                {data.slice(0, rangeValue).map((country, index) => (
+                {data
+                .filter((country)=> country.continents[0].includes(selectedRadio))
+                .sort((a, b)=> b.population - a.population)
+                .slice(0, rangeValue)
+                .map((country, index) => (
                     <Card key={index} country={country} />
                 ))}
             </ul>
