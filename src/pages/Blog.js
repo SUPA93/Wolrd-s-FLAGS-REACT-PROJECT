f8133d9bfff7dfc2843f7a5838f93024147b43a7
SUPA93 <supaflavaz93@hotmail.com>
2024-02-03 22:18:31 +0100

    Error blog text handling

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import { hasFormSubmit } from '@testing-library/user-event/dist/utils';
 
 const Blog = () => {
-    return (
-    <div className="blog-container">
-        <Logo />
-        <Navigation />
-        <h1>Blog</h1>
+    const [content, setContent] = useState("");
 
-        <form>
-            <input type="text" placeholder="Nom"/>
-            <textarea placeholder="Message"></textarea>
-            <input type="submit" value="Envoyer" />
-        </form>
-        <ul></ul>
-    </div>
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(content.length < 140){
+            setError(true);
+        } else {
+            setError(false);
+        }
+    };
+
+
+    return (
+        <div className="blog-container">
+            <Logo />
+            <Navigation />
+            <h1>Blog</h1>
+
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder="Nom" />
+                <textarea
+                style={{ border: error ? "1px solid red" : "1px solid #61dafb"}} placeholder="Message" onChangeCapture={(e) => setContent(e.target.value)}>
+                </textarea>
+                {error && <p>Minimum de 140 caractères recquis!</p>}
+                <input type="submit" value="Envoyer" />
+            </form>
+            <ul></ul>
+        </div>
     );
 };
 export default Blog;
